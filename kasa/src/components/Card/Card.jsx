f7e5f42b4fe1de0659a5b0a6ef3580df661f4e7e
Youcef Heraiz <youcefheraiz@gmail.com>
2023-04-28 16:21:57 -0400

    correction apge home

--- kasa/src/components/Card/Card.jsx
+++ kasa/src/components/Card/Card.jsx
@@ -5,6 +5,7 @@ function Card() {
   const cardList = logements.map((logement) => (
     <div className={styles.card}>
       <img className={styles.cardImage} src={logement.cover} alt={logement.title} />
+      <p className={styles.cardText}>{logement.title}</p>
     </div>
   ))
   return <div className={styles.cardContainer}>{cardList}</div>
